b529d4d19b6d7987d27c2b04e7539987272d08dc
Alessandro Toschi <aletos@plexagon.it>
2023-12-29 09:47:35 +0100

    Initial Commit

---
+++ AssetViewerSQLite/AssetViewerSQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetViewerSQLiteApp.swift
+//  AssetViewerSQLite
+//
+//  Created by Alessandro Toschi on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AssetViewerSQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
